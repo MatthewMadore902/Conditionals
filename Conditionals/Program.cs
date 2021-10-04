504c6fc6a9634edf0d2a74310ce427f503b3db49
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-04 12:30:20 -0300

    added conditionals

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -8,8 +8,43 @@ namespace Conditionals
 {
 	class Program
 	{
+		static int score = 0;
+		static int Lives = 3;
+		static bool oneUpGained = false;
 		static void Main(string[] args)
 		{
+			ShowHUD();
+
+			score = score + 1000000;
+
+			ShowHUD();
+
+			// if player did not gain a 1 up
+			if (oneUpGained == false)
+			{
+				// check for 1mil points
+				if (score >= 1000000)
+				{
+					Lives = Lives + 1; //one up rewarded 
+					oneUpGained = true;
+				}
+
+			}
+
+
+
+
+			ShowHUD();
+
+			Console.ReadKey(true);
+		}
+
+
+
+		static void ShowHUD()
+		{
+			Console.WriteLine("Score: " + score);
+			Console.WriteLine("Lives: " + Lives);
 		}
 	}
 }
